a508a6f7b42df2e0e96d3eb0300b04a2d2f72033
Caballero <kirito98.1234@gmail.com>
2023-09-04 15:20:17 -0300

    comitttt

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require ('express') ;
 const app = express () ;
-const port = 5500 ;
+const port = 8084 ;
 const morgan = require ('morgan') ;
 const path = require ('path') ;
 const bodyParser = require ('body-parser') ;
 
-app.get('/', (req , res)=>{
-    res.send('hola mundo :D')
-})
+app.set('view engine' , 'ejs' ) ;
+
+
+app.set('views', __dirname + '/views' ) ;
+
+app.use(express.static(__dirname + "/public")) ;
+
+app.get('/', (req, res) => {
+  res.render('index', { message: 'hola mundo soy un get'});
+});
+
+app.post('/', (req, res) => {
+  res.render('index', { message: 'y yo un post'});
+});
+
 
 app.listen(port,()=>{
-    console.log('servidor escuchando en http://:localhost:')
+    console.log('servidor escuchando en http://localhost:')
 })
